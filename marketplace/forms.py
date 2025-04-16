0eb7be91bb55df12c571f5150cb693611ccc2ef5
Luano van Eck <luano.vaneck@polarium.com>
2025-04-16 14:58:18 +0200

    fix: Adjusted Street Address field to use TextInput widget in registration form & username added

--- marketplace/forms.py
+++ marketplace/forms.py
@@ -47,54 +47,56 @@ class ItemForm(forms.ModelForm):
         return instance
 
 class UserRegistrationForm(forms.Form):
-                first_name = forms.CharField(max_length=30, label='First Name')
-                last_name = forms.CharField(max_length=30, label='Last Name')
-                email = forms.EmailField(label='Email Address')
-                cell_phone = forms.CharField(max_length=15, label='Cell Phone Number')
-                id_document = forms.FileField(
-                    label='Upload ID Document (Word or PDF)',
-                    required=True,
-                    widget=forms.FileInput(attrs={'accept': '.doc,.docx,.pdf'})
-                )
-                password = forms.CharField(widget=forms.PasswordInput, label='Password')
-                password_confirm = forms.CharField(widget=forms.PasswordInput, label='Confirm Password')  # Ensure this line is present
+    first_name = forms.CharField(max_length=30, label='First Name')
+    last_name = forms.CharField(max_length=30, label='Last Name')
+    email = forms.EmailField(label='Email Address')
+    cell_phone = forms.CharField(max_length=15, label='Cell Phone Number')
+    id_document = forms.FileField(
+        label='Upload ID Document (Word or PDF)',
+        required=True,
+        widget=forms.FileInput(attrs={'accept': '.doc,.docx,.pdf'})
+    )
+    password = forms.CharField(widget=forms.PasswordInput, label='Password')
+    password_confirm = forms.CharField(widget=forms.PasswordInput, label='Confirm Password')
+    username = forms.CharField(max_length=150, label='Username') # ADDED THIS LINE
 
-                # New address fields in the form
-                region = forms.CharField(max_length=100, label='Region', required=False)
-                suburb = forms.CharField(max_length=100, label='Suburb', required=False)
-                street_address = forms.CharField(widget=forms.TextInput, max_length=255, label='Street Address', required=False)
-                postal_code = forms.CharField(max_length=10, label='Postal Code', required=False)
+    # New address fields in the form
+    region = forms.CharField(max_length=100, label='Region', required=False)
+    suburb = forms.CharField(max_length=100, label='Suburb', required=False)
+    street_address = forms.CharField(widget=forms.TextInput, max_length=255, label='Street Address', required=False)
+    postal_code = forms.CharField(max_length=10, label='Postal Code', required=False)
 
-                def clean(self):
-                    cleaned_data = super().clean()
-                    password = cleaned_data.get("password")
-                    password_confirm = cleaned_data.get("password_confirm")
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        password_confirm = cleaned_data.get("password_confirm")
 
-                    if password and password_confirm and password != password_confirm:
-                        raise forms.ValidationError("Passwords do not match.")
+        if password and password_confirm and password != password_confirm:
+            raise forms.ValidationError("Passwords do not match.")
 
-                    return cleaned_data
+        return cleaned_data
 
-                def save(self, commit=True):
-                    user = User.objects.create_user(
-                        username=self.cleaned_data['email'],  # Use email as username
-                        first_name=self.cleaned_data['first_name'],
-                        last_name=self.cleaned_data['last_name'],
-                        email=self.cleaned_data['email'],
-                        password=self.cleaned_data['password']
-                    )
-                    user.save()
+    def save(self, commit=True):
+        user = User.objects.create_user(
+            username=self.cleaned_data['username'],  # changed from email to username
+            first_name=self.cleaned_data['first_name'],
+            last_name=self.cleaned_data['last_name'],
+            email=self.cleaned_data['email'],
+            password=self.cleaned_data['password']
+        )
+        user.save()
 
-                    profile = UserProfile.objects.create(user=user)
-                    profile.contact_number = self.cleaned_data['cell_phone']
-                    profile.id_document = self.cleaned_data['id_document']
-                    profile.region = self.cleaned_data['region']
-                    profile.suburb = self.cleaned_data['suburb']
-                    profile.street_address = self.cleaned_data['street_address']
-                    profile.postal_code = self.cleaned_data['postal_code']
-                    profile.save()
+        profile = UserProfile.objects.create(user=user)
+        profile.contact_number = self.cleaned_data['cell_phone']
+        profile.id_document = self.cleaned_data['id_document']
+        profile.region = self.cleaned_data['region']
+        profile.suburb = self.cleaned_data['suburb']
+        profile.street_address = self.cleaned_data['street_address']
+        profile.postal_code = self.cleaned_data['postal_code']
+        profile.save()
 
-                    return user
+        return user
+
 
     
 class PinVerificationForm(forms.Form):
